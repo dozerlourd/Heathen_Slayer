4a75c7cdea9f81283fc91790a5915cf9ac65c4dd
CHW-Y <83206220+CHW-Y@users.noreply.github.com>
2021-11-02 16:25:01 +0900

    Attack & Jump & Status Effect

--- Assets/00.MyAsset/01.Script/CHW/WeaponColCheck.cs
+++ Assets/00.MyAsset/01.Script/CHW/WeaponColCheck.cs
@@ -7,6 +7,12 @@ public class WeaponColCheck : MonoBehaviour
     [Tooltip("공격력")]
     public float attackDamage = 5f;
 
+    PlayerAttack playerAttack;
+
+    // playerAttack = playerAttack ? playerAttack : GetComponent<PlayerAttack>();
+    // playerAttack이 있으면 playerAttack을 가져오고 없으면 GetComponent를 해서 가져오는 삼항연산자
+    PlayerAttack PlayerAttack => playerAttack = playerAttack ? playerAttack : GetComponent<PlayerAttack>();
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         // 만약 충돌 대상의 태그가 Enemy면
@@ -14,6 +20,8 @@ public class WeaponColCheck : MonoBehaviour
         {
             // Enemy 체력 감소
             col.GetComponent<HPControllerToEnemy>()?.TakeDamage(attackDamage);
+
+            PlayerAttack.atkCollider.enabled = false;
         }
     }
 }
